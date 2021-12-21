02872cd4df613e4b0e99434f3815c7fc112828f5
LHWOOD <hutsonwood@gmail.com>
2021-12-21 15:23:49 -0700

    styled intro page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,1 @@
-// const mainBtn = document.querySelector("#card-btn");
 
-// function inputhandler(){
-//     event.prevent.default();
-
-// }
-
-// mainBtn.addEventListener("click", inputhandler);
