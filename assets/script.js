7a9bee04f2f63f7ef4710034e8bf84c731637823
LHWOOD <hutsonwood@gmail.com>
2021-12-22 14:54:40 -0700

    added fade in out effects to into page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
+const welCome = document.querySelector("#welcome");
+const too = document.querySelector("#to");
+const JMW = document.querySelector("#jmw");
+const fo = document.querySelector("#of");
+const btn = document.querySelector("#card-btn");
+let secondsLeft = 10;
 
+document.addEventListener("DOMContentLoaded", () => {
+  function timer() {
+    let timerInterval = setInterval(function () {
+      secondsLeft--;
+
+      if (secondsLeft === 8) {
+        welCome.classList.remove("none");
+        welCome.classList.add("inandout");
+      }
+      if (secondsLeft === 6) {
+        too.classList.remove("none");
+        too.classList.add("inandout");
+      }
+      if (secondsLeft === 4) {
+        fo.classList.remove("none");
+        fo.classList.add("inandout");
+      }
+      if (secondsLeft === 2) {
+        JMW.classList.remove("none");
+      }
+      if (secondsLeft === 0) {
+        btn.classList.remove("none");
+      }
+
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+  timer();
+});
