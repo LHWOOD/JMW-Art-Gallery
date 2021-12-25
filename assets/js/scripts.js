005298782d04fb5675e42cf9575cc80580167f47
LHWOOD <hutsonwood@gmail.com>
2021-12-25 16:30:18 -0700

    created function to enlarge img

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,21 @@
 let openMenu = document.querySelector(".menu-toggler");
 let navList = document.querySelector(".nav-list");
+let that = document.querySelector("#yoyo");
 
 openMenu.addEventListener("click", function () {
   navList.classList.toggle("active");
 });
+
+// function handle() {
+//   enlarge();
+// }
+
+// function enlarge() {
+//   if (that.height === 360) {
+//     that.classList.add("another");
+//   } else {
+//     that.classList.add("large");
+//   }
+// }
+
+// that.addEventListener("click", handle);
